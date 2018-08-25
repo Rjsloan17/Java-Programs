adaf160f7a8b1eae9ca799e2e882ad697a85aae7
Rjsloan17 <40498621+Rjsloan17@users.noreply.github.com>
2018-08-25 17:12:23 -0400

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+//Program 12_03: using different number types
+
+public class Arithmetic
+{
+    public static void main ( String args[] )
+    {
+        int i;
+        float f;
+        double d;
+        
+        int i1 = 22, i2 = 7;
+        float f1 = 22, f2 = 7;
+        
+        i = i1 / i2; System.out.println(i);
+        
+               
+    }
+}
